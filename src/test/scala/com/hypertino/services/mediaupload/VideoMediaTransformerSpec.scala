a56fa7c821b3ca724414f5a92efa181adc5be24b
Magomed Abdurakhmanov <maqdev@gmail.com>
2018-05-26 02:11:13 +0200

    - debug test code

--- src/test/scala/com/hypertino/services/mediaupload/VideoMediaTransformerSpec.scala
+++ src/test/scala/com/hypertino/services/mediaupload/VideoMediaTransformerSpec.scala
@@ -47,27 +47,27 @@ class VideoMediaTransformerSpec extends FlatSpec with Matchers {
       "1440x1080" -> "./src/test/resources/image-media-transformer/input/3.mp4-1440x1080.jpeg"
     )
   }
-
-  it should "generate dimensions ELBI" in {
-    val storageClient = new FileStorageClient("/Users/maqdev/Downloads")
-    val w = Watermark("/Users/maqdev/dev/elbi/backend/data/watermarks/elbi.png", None, None, Some(-1), Some(2), None, Some(7), percents = true)
-
-    val transformation = new Transformation {
-      def watermark: Option[Watermark] = Some(w)
-      def dimensions: Seq[Dimensions] = Seq(
-        Dimensions(Some(480), Some(848), Some(80))
-      )
-      def thumbnails: Seq[Dimensions] = Seq(
-        Dimensions(Some(480), Some(848), Some(80))
-      )
-    }
-    val tr = new VideoMediaTransformer(transformation, storageClient, "input")
-    val r = tr.transform("123.mov",Paths.get("/Users/maqdev/Downloads/123.mov"))
-    r._1 shouldBe Obj.from(
-      "480x848" -> "/Users/maqdev/Downloads/input/123-480x848.mov"
-    )
-    r._2 shouldBe Obj.from(
-      "480x848" -> "/Users/maqdev/Downloads/input/123.mov-480x848.jpeg"
-    )
-  }
+//
+//  it should "generate dimensions ELBI" in {
+//    val storageClient = new FileStorageClient("/Users/maqdev/Downloads")
+//    val w = Watermark("/Users/maqdev/dev/elbi/backend/data/watermarks/elbi.png", None, None, Some(-1), Some(2), None, Some(7), percents = true)
+//
+//    val transformation = new Transformation {
+//      def watermark: Option[Watermark] = Some(w)
+//      def dimensions: Seq[Dimensions] = Seq(
+//        Dimensions(Some(480), Some(848), Some(80))
+//      )
+//      def thumbnails: Seq[Dimensions] = Seq(
+//        Dimensions(Some(480), Some(848), Some(80))
+//      )
+//    }
+//    val tr = new VideoMediaTransformer(transformation, storageClient, "input")
+//    val r = tr.transform("123.mov",Paths.get("/Users/maqdev/Downloads/123.mov"))
+//    r._1 shouldBe Obj.from(
+//      "480x848" -> "/Users/maqdev/Downloads/input/123-480x848.mov"
+//    )
+//    r._2 shouldBe Obj.from(
+//      "480x848" -> "/Users/maqdev/Downloads/input/123.mov-480x848.jpeg"
+//    )
+//  }
 }
